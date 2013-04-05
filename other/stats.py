cdd5d904a74d3fc41490d997a05898f3d7cbf9eb
Andy Ratsirarson <andy.ratsirarson@gmail.com>
2013-04-05 09:48:40 -0500

    add mode average

--- other/stats.py
+++ other/stats.py
@@ -54,25 +54,25 @@ def mode(data):
 		else:
 			num_max = data[i]
 			maxim = 1
-			
+
 	return max_temp, data	
 	
 
 
 
-raw_data = "261 271 236 244 279 296 284 299 288 288 338 360 341 333 261 266 287 296 313 311 299 303 277 283 304 305 288 290 288 289 332 330 309 328 307 328 285 291 295 298 310 318 318 320 333 321 323 324 327 247 256 307 307 297 299 306 315"
+# raw_data = "261 271 236 244 279 296 284 299 288 288 338 360 341 333 261 266 287 296 313 311 299 303 277 283 304 305 288 290 288 289 332 330 309 328 307 328 285 291 295 298 310 318 318 320 333 321 323 324 327 247 256 307 307 297 299 306 315"
 
-data = format_data(raw_data)
+# data = format_data(raw_data)
 
-LOWEST_VALUE = min(data)
-HIGHEST_VALUE = max(data)
+# LOWEST_VALUE = min(data)
+# HIGHEST_VALUE = max(data)
 
-class_number = 5
+# class_number = 5
 
-class_width = class_width(HIGHEST_VALUE, LOWEST_VALUE, class_number)
+# class_width = class_width(HIGHEST_VALUE, class_number)
 
-class_limits = get_class_limit(data, class_width)
+# class_limits = get_class_limit(data, class_width)
 
-print class_limits
+# print class_limits
 
-print "the class width is {0}".format(class_width)
+# print "the class width is {0}".format(class_width)
